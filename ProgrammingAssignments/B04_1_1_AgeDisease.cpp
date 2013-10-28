c06640595d1a03b746952531c77cab3d2e088598
beforeload <fe.daniel91@gmail.com>
2013-10-28 18:12:25 +0800

    add first

--- ProgrammingAssignments/B04_1_1_AgeDisease.cpp
+++ ProgrammingAssignments/B04_1_1_AgeDisease.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main() {
+    int n, i;
+    cin >> n;
+    int a[n];
+    float count1 = 0, 
+        count2 = 0, 
+        count3 = 0,
+        count4 = 0;
+    for (i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] <= 18) {
+            count1++;
+        } else if (a[i] <= 35) {
+            count2++;
+        } else if (a[i] <= 60) {
+            count3++;
+        } else{
+            count4++;
+        }
+    }
     
+    cout.setf(ios::fixed);   
+    cout.setf(ios::showpoint);   
+    cout.precision(2); //用两位小数显示
+    cout << "1-18: "<< count1/n * 100 <<"%\n";
+    cout << "19-35: "<< count2/n * 100 <<"%\n";
+    cout << "36-60: "<< count3/n * 100 <<"%\n";
+    cout << "60-: "<< count4/n * 100 <<"%\n";
     return 0;
 }
